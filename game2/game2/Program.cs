41cc991d06241a355bb6dda0204d7c41eb41eb8e
jeong360 <156647038+jeong360@users.noreply.github.com>
2025-02-24 10:06:49 +0900

    Ignore .vs folder

---
+++ game2/game2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace game2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //변수선언
+            int age; // 나이,정수형 변수
+            age = 25; //변수에 값 저장
+
+            console.WriteLine(age); //변수에 저장된 값을 출력 25
+
+            //컨트롤 + s 누르면 저장됨
+
+
+        }
+    }
+}
